006ac4636d12370f6b4810e55e7c5305b55f6806
Creckeryop <didager@yandex.ru>
2018-06-13 22:14:06 +0500

    Added levelReader function

--- index.lua
+++ index.lua
@@ -10,7 +10,21 @@ for i = 1, #Libs do
 end
 palette = Graphics.loadImage(dataDir.."palette.png")
 paletteTable = {}
-level = {}
+level = {
+	name = "Pikachu",
+	width = 10,
+	height = 10,
+	map = {	1,0,0,1,0,0,0,0,0,1,
+			1,1,0,1,1,0,0,0,1,1,
+			0,1,1,0,1,1,1,1,1,0,
+			0,0,1,1,0,0,0,0,0,1,
+			0,0,1,0,0,0,0,0,0,0,
+			0,0,1,0,1,0,0,0,0,1,
+			0,1,0,1,1,0,0,0,1,1,
+			0,1,1,0,0,0,1,0,0,0,
+			0,1,1,0,0,1,1,1,0,0,
+			0,0,1,1,0,0,0,0,0,1}
+}
 local DeltaTimer, newTime = Timer.new(), 0
 local Controls_check = Controls.check
 local Color_new = Color.new
@@ -35,6 +49,22 @@ local function Scan_Palette(pal_tex)
 		Screen.flip()
 	end
 end
+local function drawRect(x, y, w, h, c)
+	Graphics.fillRect(x, x + w, y, y + h, c)
+end
+local function drawLevel()
+	drawRect(0,0,24*level.width,24*level.height, Color_new(0, 0, 0))
+	local y = 0
+	for i = 0, level.width - 1 do
+		local x = 0
+		for j = 0, level.height - 1 do
+			local now = 1 - level.map[i*level.width+j+1]
+			drawRect(x, y, 22, 22, Color_new(255*now, 255*now, 255*now))
+			x = x + 24
+		end
+		y = y + 24
+	end
+end
 Scan_Palette(palette)
 while true do
 	dt = newTime / 8
@@ -42,7 +72,7 @@ while true do
 	pad = Controls.read()
 	Graphics.initBlend()
 	Screen.clear()
-	
+	drawLevel()
 	Graphics.termBlend()
 	if Controls_click(SCE_CTRL_SELECT) then
 		FontLib_close()
